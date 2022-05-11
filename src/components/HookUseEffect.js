edce54de912124b5a8832e4d84f55841aeff9c92
Alice <alicelobinha2@gmail.com>
2022-05-11 15:57:57 -0300

    useReducer

---
+++ src/components/HookUseEffect.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HookUseEffect = () => {
+    // 1- useEffect, sem dependências
+    return (
+        <div>
+            <h2>useEffect</h2>
+            <hr />
+        </div>
+    )
+}
+
+export default HookUseEffect
