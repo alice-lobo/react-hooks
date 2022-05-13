b2938fb40683ea5e8317d36caa1b660e32953c46
Alice <alicelobinha2@gmail.com>
2022-05-13 13:41:07 -0300

    useEffect

--- src/components/HookUseEffect.js
+++ src/components/HookUseEffect.js
@@ -28,6 +28,17 @@ const HookUseEffect = () => {
     }, [anotherNumber]);
 
     const changeNumber = () => setAnotherNumber(anotherNumber + 1);
+
+    // 4 - cleanup do useEffect
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            console.log("Hello World");
+            setAnotherNumber(anotherNumber + 1);
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, [anotherNumber]);
+
     return (
         <div>
             <h2>useEffect</h2>
