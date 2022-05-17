529560df17a843f3538b769036c7f8455e6e1129
Alice <alicelobinha2@gmail.com>
2022-05-16 22:52:31 -0300

    useCallback

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,12 @@ import HookUseEffect from "../components/HookUseEffect";
 import HookUseReducer from "../components/HookUseReducer";
 import HookUseState from "../components/HookUseState";
 import HookUseRef from "../components/HookUseRef";
+import HookUseCallback from "../components/HookUseCallback";
 
 // useContext
 import { useContext } from "react";
 import { SomeContext } from "../components/HookUseContext";
+
 
 const Home = () => {
     const { contextValue } = useContext(SomeContext);
@@ -19,6 +21,7 @@ const Home = () => {
             <p>Valor do context: {contextValue}</p>
             <hr />
             <HookUseRef />
+            <HookUseCallback />
         </div>
     )
 }
