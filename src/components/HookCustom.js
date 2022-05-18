fe60116c6f1a2e55eaf77e63bce867615558d54e
Alice <alicelobinha2@gmail.com>
2022-05-18 15:16:33 -0300

    usePrevious

--- src/components/HookCustom.js
+++ src/components/HookCustom.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { usePrevious } from "../hooks/usePrevious";
 
 const HookCustom = () => {
+
+    const [number, setNumber] = useState(0);
+    const previousValue = usePrevious(number);
+
     return (
         <div>
             <h2>Custom Hook</h2>
+            <p>Atual: {number}</p>
+            <p>Anterior: {previousValue}</p>
+            <button onClick={() => setNumber(Math.random())}>Alterar!</button>
             <hr />
         </div>
     )
--- src/hooks/usePrevious.js
+++ src/hooks/usePrevious.js
@@ -3,7 +3,7 @@ import { useEffect, useRef } from "react";
 export const usePrevious = (value) => {
     const ref = useRef;
 
-    useEffect(() => {
+    useEffect(() => { //chamado tda vez q altera
         ref.current = value;
     });
 
